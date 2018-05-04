6047c0758a269dd0ce1d056d0fd172fb1e625e00
unknown <staryob@gmail.com>
2018-05-04 20:48:24 +0700

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var bodyParser = require('body-parser')
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(process.env.PORT || 3000);
@@ -7,4 +8,38 @@ app.listen(process.env.PORT || 3000);
 app.get("/", function(req, res){
 	res.render("trangchu");
 });
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.post('/phimmoi', urlencodedParser, function (req, res) {
+  var mahoa = req.body.mahoa;
+  if (typeof Buffer.from === "function") {
+    // Node 5.10+
+    var buf = Buffer.from(mahoa, 'base64'); // Ta-da
+	} else {
+	    // older Node versions
+	    var buf = new Buffer(mahoa, 'base64'); // Ta-da
+	};
+  res.send(unpack(buf));
+});
+
+
+ function unpack(mahoa){
+    var c=mahoa;
+    var a=10,x=1;
+    while(x<a){
+        c=unescape(c);
+        if(/eval\(+function\(/.test(c))
+        {
+            c=depack(c);x++
+        }else{break}
+    };
+    c=unescape(c);
+    return c
+}
+
+
+
+
+function depack(p)
+{if(p!=""){c=unescape(p);var _e=eval,s="eval=function(v){c=v;};"+c+";eval=_e;";eval(s)}else{c=p};return c}
 
